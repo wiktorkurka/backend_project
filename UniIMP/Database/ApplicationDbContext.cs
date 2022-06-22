ede485b98743ec2bde68f45de67e64c0d4822c56
Wiktor Kurka <wiktor-kurka@protonmail.com>
2022-06-22 17:11:59 +0200

    Base database implementation

---
+++ UniIMP/Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UniIMP.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
