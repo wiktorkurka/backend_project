30bc6a5305127b3a0d80d6bbf47eb1ccd6cccc36
Wiktor Kurka <wiktor-kurka@protonmail.com>
2022-07-07 12:23:19 +0200

    Rename controller to match convention

--- UniIMP/Controllers/API/AgentsController.cs
+++ UniIMP/Controllers/API/AgentsController.cs
@@ -1,23 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using UniIMP.DataAccess;
 using UniIMP.DataAccess.Entities;
 using UniIMP.DataAccess.Repositories;
+using UniIMP.Models;
 using UniIMP.Services;
 
 namespace UniIMP.Controllers.API
 {
-    public class AgentController : CrudController<SnmpAgent>
+    public class AgentsController : CrudController<SnmpAgent>
     {
         private readonly ICrudRepository<SnmpAgent> _agentRepository;
+        private readonly ICrudRepository<Asset> _assetRepository;
         private readonly SnmpPollerService _snmpPollerService;
 
-        public AgentController(
+        public AgentsController(
             ApplicationDbContext dbContext,
             ICrudRepository<SnmpAgent> agentRepository,
+            ICrudRepository<Asset> assetRepository,
             SnmpPollerService snmpPollerService)
             : base(agentRepository)
         {
             _agentRepository = agentRepository;
+            _assetRepository = assetRepository;
             _snmpPollerService = snmpPollerService;
         }
 
@@ -32,6 +37,24 @@ namespace UniIMP.Controllers.API
                 await _snmpPollerService.PollAsync(id);
 
             return Ok();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Post([FromBody] SnmpAgentIntermediateModel model) {
+            try {
+            SnmpAgent agent = new SnmpAgent() {
+                Asset = await _assetRepository.GetAsync(model.AssetId),
+                IpAddress = IPAddress.Parse(model.IpAddress),
+                Community = model.Community
+            };
+            
+            await _agentRepository.CreateAsync(agent);
+            await _agentRepository.SaveAsync();
+
+            return Ok();
+            } catch (FormatException) {
+                return BadRequest(model);
+            }
         }
     }
 }
